67f2efd2a6eea001c8ad159ea68b1e0294a780b9
Tronde <tronde@my-it-brain.de>
2015-11-01 21:33:05 +0100

    Build-Script ready for testing.

--- build_fflip_fw.sh
+++ build_fflip_fw.sh
@@ -8,9 +8,10 @@
 # Variables ###################################################################
 RELEASE="v2015.1.2"
 DIR=`pwd`
-# SITES=(`ls $DIR/sites`)
-SITES=(BO BS LIP) # Used for testing
+SITES=(`ls $DIR/sites`)
+# SITES=(BO BS LIP) # Used for testing
 CORES=2 # Specifies the number of jobs (commands) to run simultaneously.
+SECRET=$DIR/secret
 ###############################################################################
 
 git clone https://github.com/freifunk-gluon/gluon.git gluon -b $RELEASE
@@ -30,7 +31,7 @@ for SITE in "${SITES[@]}"
     make -j$CORES GLUON_BRANCH=stable GLUON_TARGET=ar71xx-nand # V=s
     make -j$CORES GLUON_BRANCH=stable GLUON_TARGET=mpc85xx-generic # V=s
     make manifest GLUON_BRANCH=stable
-#    ./contrib/sign.sh secret images/sysupgrade/stable.manifest
+    ./contrib/sign.sh $SECRET images/sysupgrade/stable.manifest
     mkdir -p images/$SITE
     mv images/factory images/$SITE/
     mv images/sysupgrade images/$SITE/
