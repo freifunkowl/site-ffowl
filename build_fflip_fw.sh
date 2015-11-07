479e518d6fc4d78f9168d1e99dc7f96c08e66b2b
Michael Brinkmann <michael@collimas.net>
2015-11-07 16:24:52 +0100

    Added copy command for sign.sh due to changed ecdsautil syntax
    And also added creation and signing commands for experimental branch. Necessary to get rid of this branch in the wild!

--- build_fflip_fw.sh
+++ build_fflip_fw.sh
@@ -16,6 +16,7 @@ SECRET=$DIR/secret
 
 rm -rf gluon
 git clone https://github.com/freifunk-gluon/gluon.git gluon -b $RELEASE
+cp $DIR/sign.sh $DIR/gluon/contrib/
 mkdir gluon/site
 # cp $DIR/secret $DIR/gluon/
 
@@ -23,7 +24,7 @@ for SITE in "${SITES[@]}"
   do
     cp $DIR/sites/$SITE/site.* $DIR/gluon/site/
     cd gluon
-#    git pull origin $RELEASE	# Unnötig, wird beim Repository Start geklont. 
+#    git pull origin $RELEASE	# UnnÃ¶tig, wird beim Repository Start geklont. 
     make update
     make clean GLUON_TARGET=ar71xx-generic
     make clean GLUON_TARGET=ar71xx-nand
@@ -32,7 +33,9 @@ for SITE in "${SITES[@]}"
     make -j$CORES GLUON_BRANCH=stable GLUON_TARGET=ar71xx-nand # V=s
     make -j$CORES GLUON_BRANCH=stable GLUON_TARGET=mpc85xx-generic # V=s
     make manifest GLUON_BRANCH=stable
+    make manifest GLUON_BRANCH=experimental
     ./contrib/sign.sh $SECRET images/sysupgrade/stable.manifest
+    ./contrib/sign.sh $SECRET images/sysupgrade/experimental.manifest
     mkdir -p images/$SITE
     mv -f images/factory images/$SITE/
     mv -f images/sysupgrade images/$SITE/
