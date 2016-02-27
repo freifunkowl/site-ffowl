508e63fb494df39031146d28fed6befd2ae11f7a
Michael Brinkmann <michael@collimas.net>
2016-02-27 13:03:11 +0100

    Added Extertal

--- mksites.sh
+++ mksites.sh
@@ -9,8 +9,8 @@
 # Freifunk-Communities von Freifunk-Lippe.
 ###############################################################################
 
-SITES=(bs dt ff hb kt la le lh lip sc oe)
-SSIDS=(badsalzuflen detmold Freifunk hornbadmeinberg kalletal lage lemgo leopoldshoehe lippe schoetmar oerlinghausen)
+SITES=(bs dt ff hb kt la le lh lip sc oe ex)
+SSIDS=(badsalzuflen detmold Freifunk hornbadmeinberg kalletal lage lemgo leopoldshoehe lippe schoetmar oerlinghausen extertal)
 typeset -i i=0
 
 for SITE in "${SITES[@]}"
