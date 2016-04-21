ab55dd85284f05532f5f8da0f6e30d51dc17ed5d
Tronde <tronde@my-it-brain.de>
2016-04-21 17:45:23 +0200

    Optimized if-condition and inserted functions for branch building

--- build_fflip_fw.sh
+++ build_fflip_fw.sh
@@ -15,83 +15,90 @@ CORES=3 # Specifies the number of jobs (commands) to run simultaneously.
 SECRET=$DIR/secret
 ###############################################################################
 
-$DIR/mksites.sh
+# Functions ###################################################################
+build_stable_branch(){
+  for SITE in "${SITES[@]}"
+    do
+      cp $DIR/sites/$SITE/site.* $DIR/gluon/site/
+      cp -r $DIR/i18n/ $DIR/gluon/site/
+      cd $DIR/gluon/
+      # This creates images for the stable branch
+      time make -j$CORES BROKEN=1 GLUON_BRANCH=stable GLUON_TARGET=ar71xx-generic V=99 2>&1 | tee make_$SITE_$(date +%y%m%d_%H%M).log
+      time make -j$CORES BROKEN=1 GLUON_BRANCH=stable GLUON_TARGET=ar71xx-nand V=99 2>&1 | tee make_$SITE_$(date +%y%m%d_%H%M).log
+      time make -j$CORES BROKEN=1 GLUON_BRANCH=stable GLUON_TARGET=mpc85xx-generic V=99 2>&1 | tee make_$SITE_$(date +%y%m%d_%H%M).log
+      make manifest GLUON_BRANCH=stable
+      ./contrib/sign.sh $SECRET output/images/sysupgrade/stable.manifest   
+      mkdir -p output/images/$SITE
+      mv -f output/images/factory output/images/$SITE/
+      mv -f output/images/sysupgrade output/images/$SITE/
+      rm $DIR/gluon/site/site.*
+      mkdir $DIR/gluon/output/images/logs
+      mv $DIR/gluon/make*.log output/images/logs
+  done
+}
 
-# If gluon directory exists update to latest master and push this into actual repo
-if [ -d "$DIR/gluon" ]; then
-  cd $DIR/gluon
-  cp $DIR/sites/ff/site.* $DIR/gluon/site/
-  make update
-  git pull
-  git submodule update --remote gluon
-fi
+build_experimantal_branch(){
+  for SITE in "${SITES[@]}"
+    do
+      cp $DIR/sites-experimental/$SITE/site.* $DIR/gluon/site/
+      cp -r $DIR/i18n/ $DIR/gluon/site/
+      cd $DIR/gluon/
+      time make -j$CORES BROKEN=1 GLUON_BRANCH=experimental GLUON_TARGET=brcm2708-bcm2708 V=99 2>&1 | tee make_$SITE_$(date +%y%m%d_%H%M).log
+      time make -j$CORES BROKEN=1 GLUON_BRANCH=experimental GLUON_TARGET=brcm2708-bcm2709 V=99 2>&1 | tee make_$SITE_$(date +%y%m%d_%H%M).log
+      time make -j$CORES BROKEN=1 GLUON_BRANCH=experimental GLUON_TARGET=sunxi V=99 2>&1 | tee make_$SITE_$(date +%y%m%d_%H%M).log
+      time make -j$CORES BROKEN=1 GLUON_BRANCH=experimental GLUON_TARGET=x86-64 V=99 2>&1 | tee make_$SITE_$(date +%y%m%d_%H%M).log
+      time make -j$CORES BROKEN=1 GLUON_BRANCH=experimental GLUON_TARGET=x86-generic V=99 2>&1 | tee make_$SITE_$(date +%y%m%d_%H%M).log
+      mkdir -p output/images-experimental/$SITE
+      mv -f output/images/factory output/images-experimental/$SITE/
+      mv -f output/images/sysupgrade output/images-experimental/$SITE/
+      rm $DIR/gluon/site/site.*
+      mkdir $DIR/gluon/output/images-experimental/logs
+      mv $DIR/gluon/make*.log output/images-experimental/logs
+  done
+}
+###############################################################################
 
-# If gluon directory does not exist do a fresh clone frome the Freifunk-Gluon Repo
-if [ ! -d "$DIR/gluon" ]; then
-  cd $DIR
-  git clone https://github.com/freifunk-gluon/gluon.git gluon # -b $RELEASE
-  mkdir $DIR/gluon/site
-  cp $DIR/sites/ff/site.* $DIR/gluon/site/
-  cd $DIR/gluon
-  make update
-fi
+# Main ########################################################################
+tar -czvf $DIR/gluon/output/buildlog_experimental.tar $DIR/gluon/output/images-experimental/logs
+rm -rf $DIR/gluon/output/images-experimental/logs
 
-cp $DIR/sign.sh $DIR/gluon/contrib/
-
-if [ ! -d "$DIR/gluon/site" ]; then
-  mkdir $DIR/gluon/site
-fi
-
-cp $DIR/modules $DIR/gluon/site/
-
-for SITE in "${SITES[@]}"
-  do
-    cp $DIR/sites/$SITE/site.* $DIR/gluon/site/
-    cp -r $DIR/i18n/ $DIR/gluon/site/
-    cd $DIR/gluon/
-   # This creates images for the stable branch
-    time make -j$CORES BROKEN=1 GLUON_BRANCH=stable GLUON_TARGET=ar71xx-generic V=99 2>&1 | tee make_$SITE_$(date +%y%m%d_%H%M).log
-    time make -j$CORES BROKEN=1 GLUON_BRANCH=stable GLUON_TARGET=ar71xx-nand V=99 2>&1 | tee make_$SITE_$(date +%y%m%d_%H%M).log
-    time make -j$CORES BROKEN=1 GLUON_BRANCH=stable GLUON_TARGET=mpc85xx-generic V=99 2>&1 | tee make_$SITE_$(date +%y%m%d_%H%M).log
-    make manifest GLUON_BRANCH=stable
-    ./contrib/sign.sh $SECRET output/images/sysupgrade/stable.manifest   
-    mkdir -p output/images/$SITE
-    mv -f output/images/factory output/images/$SITE/
-    mv -f output/images/sysupgrade output/images/$SITE/
-    rm $DIR/gluon/site/site.*
-    mkdir $DIR/gluon/output/images/logs
-    mv $DIR/gluon/make*.log output/images/logs
-done
+echo "Finished building Experimental branch."
 
 tar -czvf $DIR/gluon/output/buildlog_stable.tar $DIR/gluon/output/images/logs
 rm -rf $DIR/gluon/output/images/logs
 
 echo "Finished building Stable branch."
 
+
+$DIR/mksites.sh
+
+# If gluon directory exists update to latest master and push this into actual repo
+if [ -d "$DIR/gluon" ]
+  then
+    cd $DIR/gluon
+      if [ ! -d "$DIR/gluon/site" ]; then
+        mkdir $DIR/gluon/site
+      fi
+    cp $DIR/sites/ff/site.* $DIR/gluon/site/
+    make update
+    git pull
+    git submodule update --remote gluon
+  else
+    # If gluon directory does not exist do a fresh clone frome the Freifunk-Gluon Repo
+    cd $DIR
+    git clone https://github.com/freifunk-gluon/gluon.git gluon # -b $RELEASE
+    mkdir $DIR/gluon/site
+    cp $DIR/sites/ff/site.* $DIR/gluon/site/
+    cd $DIR/gluon
+    make update
+fi
+
+cp $DIR/sign.sh $DIR/gluon/contrib/
+cp $DIR/modules $DIR/gluon/site/
+build_stable_branch
+
 # This creates images for the Experimental branch
 # Uncomment if you want to build Experimental images
-
-for SITE in "${SITES[@]}"
-  do
-    cp $DIR/sites-experimental/$SITE/site.* $DIR/gluon/site/
-    cp -r $DIR/i18n/ $DIR/gluon/site/
-    cd $DIR/gluon/
-    time make -j$CORES BROKEN=1 GLUON_BRANCH=experimental GLUON_TARGET=brcm2708-bcm2708 V=99 2>&1 | tee make_$SITE_$(date +%y%m%d_%H%M).log
-    time make -j$CORES BROKEN=1 GLUON_BRANCH=experimental GLUON_TARGET=brcm2708-bcm2709 V=99 2>&1 | tee make_$SITE_$(date +%y%m%d_%H%M).log
-    time make -j$CORES BROKEN=1 GLUON_BRANCH=experimental GLUON_TARGET=sunxi V=99 2>&1 | tee make_$SITE_$(date +%y%m%d_%H%M).log
-    time make -j$CORES BROKEN=1 GLUON_BRANCH=experimental GLUON_TARGET=x86-64 V=99 2>&1 | tee make_$SITE_$(date +%y%m%d_%H%M).log
-    time make -j$CORES BROKEN=1 GLUON_BRANCH=experimental GLUON_TARGET=x86-generic V=99 2>&1 | tee make_$SITE_$(date +%y%m%d_%H%M).log
-    mkdir -p output/images-experimental/$SITE
-    mv -f output/images/factory output/images-experimental/$SITE/
-    mv -f output/images/sysupgrade output/images-experimental/$SITE/
-    rm $DIR/gluon/site/site.*
-    mkdir $DIR/gluon/output/images-experimental/logs
-    mv $DIR/gluon/make*.log output/images-experimental/logs
-done
-
-tar -czvf $DIR/gluon/output/buildlog_experimental.tar $DIR/gluon/output/images-experimental/logs
-rm -rf $DIR/gluon/output/images-experimental/logs
-
-echo "Finished building Experimental branch."
+# build_experimantal_branch
 
 exit
