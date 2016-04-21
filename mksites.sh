d15788e3d2df7dd31b0eb538c8558e8ce5ebfa18
Tronde <tronde@my-it-brain.de>
2016-04-21 18:04:07 +0200

    Funktionen eingebaut, um Ablauf besser steuern zu können.

--- mksites.sh
+++ mksites.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 ###############################################################################
-# Autor: Tronde (tronde(at)my-it-brain(dot)de)
+# Autor: Tronde (tronde(at)my-it-brain(dot)de) Datum: 2015-11-11
 # Modified by: Collimas 2016-04-20
-# Datum: 2015-11-11
 # Lizenz: GPLv3
 #
 # Beschreibung:
@@ -10,36 +9,49 @@
 # Freifunk-Communities von Freifunk-Lippe.
 ###############################################################################
 
+# Variablen ###################################################################
 SITES=(bs dt ff hb kt la le lh lip sc oe ex ad)
 SSIDS=(badsalzuflen detmold Freifunk hornbadmeinberg kalletal lage lemgo leopoldshoehe lippe schoetmar oerlinghausen extertal augustdorf)
 typeset -i i=0
 
+# Funktionen ##################################################################
 # Erzeuge Configs fuer Stable-Branch
-for SITE in "${SITES[@]}"
-  do
-    sed "s/\<lip\>/$SITE/g" site.conf.example | sed "/ssid/s/\<lippe\>/${SSIDS[i]}/g" > sites/$SITE/site.conf
-    sed "s/\<lip\>/$SITE/g" site.mk.example > sites/$SITE/site.mk
-    i=$i+1
-done
+create_stable_configs(){
+  for SITE in "${SITES[@]}"
+    do
+      sed "s/\<lip\>/$SITE/g" site.conf.example | sed "/ssid/s/\<lippe\>/${SSIDS[i]}/g" > sites/$SITE/site.conf
+      sed "s/\<lip\>/$SITE/g" site.mk.example > sites/$SITE/site.mk
+      i=$i+1
+  done
+}
 
 # Sonderfall SSID Freifunk - Stable
-mv sites/ff/site.conf sites/ff/site-tmp.conf
-sed "s/\<Freifunk.freifunk.net\>/Freifunk/g" sites/ff/site-tmp.conf > sites/ff/site.conf
-rm -f sites/ff/site-tmp.conf
-
-SITES=(bs dt ff hb kt la le lh lip sc oe ex ad)
-SSIDS=(badsalzuflen detmold Freifunk hornbadmeinberg kalletal lage lemgo leopoldshoehe lippe schoetmar oerlinghausen extertal augustdorf)
-typeset -i i=0
+create_stable_ssid_freifunk(){
+  mv sites/ff/site.conf sites/ff/site-tmp.conf
+  sed "s/\<Freifunk.freifunk.net\>/Freifunk/g" sites/ff/site-tmp.conf > sites/ff/site.conf
+  rm -f sites/ff/site-tmp.conf
+}
 
 # Erzeuge Configs fuer Experimental-Branch
-for SITE in "${SITES[@]}"
-  do
-    sed "s/\<lip\>/$SITE/g" site.conf.experimental.example | sed "/ssid/s/\<lippe\>/${SSIDS[i]}/g" > sites-experimental/$SITE/site.conf
-    sed "s/\<lip\>/$SITE/g" site.mk.experimental.example > sites-experimental/$SITE/site.mk
+create_experimental_config(){
+  for SITE in "${SITES[@]}"
+    do
+      sed "s/\<lip\>/$SITE/g" site.conf.experimental.example | sed "/ssid/s/\<lippe\>/${SSIDS[i]}/g" > sites-experimental/$SITE/site.conf
+      sed "s/\<lip\>/$SITE/g" site.mk.experimental.example > sites-experimental/$SITE/site.mk
     i=$i+1
-done
+  done
+}
 
 # Sonderfall SSID Freifunk - Experimental
-mv sites-experimental/ff/site.conf sites-experimental/ff/site-tmp.conf
-sed "s/\<Freifunk.freifunk.net\>/Freifunk/g" sites-experimental/ff/site-tmp.conf > sites-experimental/ff/site.conf
-rm -f sites-experimental/ff/site-tmp.conf
+create_experimental_ssid_freifunk(){
+  mv sites-experimental/ff/site.conf sites-experimental/ff/site-tmp.conf
+  sed "s/\<Freifunk.freifunk.net\>/Freifunk/g" sites-experimental/ff/site-tmp.conf > sites-experimental/ff/site.conf
+  rm -f sites-experimental/ff/site-tmp.conf
+}
+
+# Hauptprogramm ###############################################################
+create_stable_configs
+create_stable_ssid_freifunk
+create_experimental_config
+create_experimental_ssid_freifunk
+exit
